15508ca7e1a3bb7cc3c918f6491dda619e8b4a2b
Darren Sagucio <darrensagucio@Darrens-MacBook-Pro.local>
2021-02-23 12:00:26 -0800

    finished. need to write comments

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -33,17 +33,17 @@ percentli = percentage(totalli, numofvotes)
 totalotooley = nameandvote.count("O'Tooley")
 percentotooley = percentage(totalotooley, numofvotes) 
 
-nameofcandiate = ["Khan","Correy","Li","O'Tooley"]
+nameofcandidate = ["Khan","Correy","Li","O'Tooley"]
 percentvotes = [percentkhan,percentcorrey,percentli,percentotooley]
-eachtotalcandiatevotes = [totalkhan,totalcorrey,totalli,totalotooley]
+eachtotalcandidatevotes = [totalkhan,totalcorrey,totalli,totalotooley]
 
-highestvote = max(eachtotalcandiatevotes)
+highestvote = max(eachtotalcandidatevotes)
 
-for row in range(0,len(eachtotalcandiatevotes)):
-    if eachtotalcandiatevotes[row] != highestvote: 
+for row in range(0,len(eachtotalcandidatevotes)):
+    if eachtotalcandidatevotes[row] != highestvote: 
         pass
     else: 
-        winner = nameofcandiate[row]
+        winner = nameofcandidate[row]
 
 
 print("' ' ' text")
@@ -53,11 +53,34 @@ print ("-")
 print (f"Total Votes: {numofvotes}")
 print ("----------------------") 
 print ("-") 
-for row in range(0,len(nameofcandiate)):
-    print (f"{nameofcandiate[row]}: {percentvotes[row]}% ({eachtotalcandiatevotes[row]})")
+for row in range(0,len(nameofcandidate)):
+    print (f"{nameofcandidate[row]}: {percentvotes[row]}% ({eachtotalcandidatevotes[row]})")
 print ("----------------------") 
 print ("-") 
 print(f"Winner: {winner}")
 print ("----------------------") 
 print ("-") 
 print("' ' '")
+
+totalnumstring = " {}".format(numofvotes)
+numofkhan = " {}% ({}) ".format(percentvotes[0],eachtotalcandidatevotes[0])
+numofcorrey = " {}% ({}) ".format(percentvotes[1],eachtotalcandidatevotes[1])
+numofli = " {}% ({}) ".format(percentvotes[2],eachtotalcandidatevotes[2])
+numofotooley = " {}% ({}) ".format(percentvotes[3],eachtotalcandidatevotes[3])
+winnerstring = " {}".format(winner) 
+
+analysis = ["Total Votes: ", "Khan: ", "Correy: ", "Li: ", "O'Tooley: ", "Winner: "]
+numresults = [totalnumstring, numofkhan, numofcorrey, numofli,numofotooley,winnerstring]
+
+results = zip(analysis, numresults)
+
+export_file = os.path.join('analysis','analysis.csv')
+
+with open(export_file, "w", newline = '') as analysisfile:
+    writer = csv.writer(analysisfile)
+
+    writer.writerow(["Analyis"," Results"])
+    
+    writer.writerows(results)
+
+print("pass")
