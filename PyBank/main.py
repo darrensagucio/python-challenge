9e6d1158f444107a09b26428965b715e82899e80
Darren Sagucio <darrensagucio@Darrens-MacBook-Pro.local>
2021-02-22 23:41:41 -0800

    Average Changes

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,6 +6,7 @@ budget_data_csv = os.path.join('Resources', 'budget_data.csv')
 
 date = []
 profit = []
+changes = []
 
 with open(budget_data_csv, 'r') as csvfile: 
 
@@ -18,9 +19,31 @@ with open(budget_data_csv, 'r') as csvfile:
 
         profit.append(row[1])
 
-numofmonths = len(date)
 
+numofmonths = len(date)
 print(f"Total Months: {numofmonths}")
+
+#https://stackoverflow.com/questions/7368789/convert-all-strings-in-a-list-to-int
+profit = [int(i) for i in profit]
+
+#https://www.geeksforgeeks.org/python-program-to-find-sum-of-elements-in-list/
+totalprofit = 0 
+difference = 0 
+for row in range(0,len(profit)):
+    totalprofit = totalprofit + profit[row]
+    if row != len(profit) - 1:
+        difference = profit[row + 1] - profit[row]
+        changes.append(difference)
+    else:
+        break
+
+print(f"Total: ${totalprofit}")
+
+average = sum(changes) / len(changes)
+average = round(average, 2)
+print(f"Average Changes: ${average}")
+
+
    
 
 print("pass")
