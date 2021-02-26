3666a9af2810abe9df82ccc1f61507c2eb27eb24
Darren Sagucio <darrensagucio@Darrens-MacBook-Pro.local>
2021-02-26 07:59:49 -0800

    Commented

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,26 +1,34 @@
 import os 
 import csv 
+# Imported Decimal Package
 from decimal import *
 
 election_data_csv = os.path.join('Resources', 'election_data.csv')
 
+# Created List
 nameandvote = []
 
+# Read CSV File
 with open(election_data_csv, 'r') as csvfile: 
 
     csvreader = csv.reader(csvfile, delimiter = ',')
+    # Skips Header
     header = next(csvreader) 
 
     for row in csvreader: 
         nameandvote.append(row[2])
 
+# Length Of List
 numofvotes = len(nameandvote)
+
+# Percentage Function 
 def percentage(totalvotes, allvotes):
     percent = (totalvotes / allvotes)*100
     percent = Decimal(percent)
     percent = round(percent, 3)
     return percent
 
+# Counts Unique Votes And Percentage Within The Election For Each Candidate
 totalkhan = nameandvote.count("Khan")
 percentkhan = percentage(totalkhan, numofvotes)
 
@@ -33,10 +41,12 @@ percentli = percentage(totalli, numofvotes)
 totalotooley = nameandvote.count("O'Tooley")
 percentotooley = percentage(totalotooley, numofvotes) 
 
+# Creating New Lists 
 nameofcandidate = ["Khan","Correy","Li","O'Tooley"]
 percentvotes = [percentkhan,percentcorrey,percentli,percentotooley]
 eachtotalcandidatevotes = [totalkhan,totalcorrey,totalli,totalotooley]
 
+# Finding Winner
 highestvote = max(eachtotalcandidatevotes)
 
 for row in range(0,len(eachtotalcandidatevotes)):
@@ -53,6 +63,7 @@ print ("-")
 print (f"Total Votes: {numofvotes}")
 print ("----------------------") 
 print ("-") 
+# For Loop To Print Candidate Name, Percentage, And Number Of Votes
 for row in range(0,len(nameofcandidate)):
     print (f"{nameofcandidate[row]}: {percentvotes[row]}% ({eachtotalcandidatevotes[row]})")
 print ("----------------------") 
@@ -62,6 +73,7 @@ print ("----------------------")
 print ("-") 
 print("' ' '")
 
+# Creating String Variables (https://matthew-brett.github.io/teaching/string_formatting.html)
 totalnumstring = " {}".format(numofvotes)
 numofkhan = " {}% ({}) ".format(percentvotes[0],eachtotalcandidatevotes[0])
 numofcorrey = " {}% ({}) ".format(percentvotes[1],eachtotalcandidatevotes[1])
@@ -70,10 +82,12 @@ numofotooley = " {}% ({}) ".format(percentvotes[3],eachtotalcandidatevotes[3])
 winnerstring = " {}".format(winner) 
 
 analysis = ["Total Votes: ", "Khan: ", "Correy: ", "Li: ", "O'Tooley: ", "Winner: "]
+# Inserting String Variables Into Lists
 numresults = [totalnumstring, numofkhan, numofcorrey, numofli,numofotooley,winnerstring]
 
 results = zip(analysis, numresults)
 
+# Exporting To A CSV File
 export_file = os.path.join('analysis','analysis.csv')
 
 with open(export_file, "w", newline = '') as analysisfile:
@@ -82,4 +96,3 @@ with open(export_file, "w", newline = '') as analysisfile:
     writer.writerow(["Analyis"," Results"])
     
     writer.writerows(results)
-
